e8de8562e34f43b2acbf7806a753a7fe591dc379
Ivan Kan <ivankan@outlook.com>
2015-08-16 16:54:33 -0700

    Begun CPP file

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,16 @@
+// Code for Project 2
+// Go for broke
+// Ivan Kan
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	
+	
+	system("PAUSE");
+	return 0;
+}
